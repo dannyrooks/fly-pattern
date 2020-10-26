75ff0d106b6cac051504fbdd03b579eac6689633
Daniel Rooker <dannyrooks@gmail.com>
2020-10-26 00:48:05 -0400

    patternContainer

--- src/containers/patternContainer.js
+++ src/containers/patternContainer.js
@@ -2,23 +2,33 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { addPattern } from '../actions/fetchPatterns'
 
-class PatternForm extends Component {
-    
+class AddPattern extends Component {
     state = {
         pattern: {
             name: "",
-            description: ""
+            category: "",
+            comment: ""
         },
         loading: false
+    }
+
+    handleChange = (event) => {
+
+    }
+
+    handleSubmit = (event) => {
+
     }
 
     render() {
         return (
             <div>
-                
+                <form onSubmit={this.handleSubmit}>
+
+                </form>
             </div>
         )
     }
 }
 
-export default connect(null, { addPattern })(patternForm)
+export default connect(null, { addPattern })(AddPattern)
