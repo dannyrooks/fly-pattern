a6bc84ca29d8462a838782b1eb5d1e0a23b53632
Daniel Rooker <dannyrooks@gmail.com>
2020-10-26 13:27:48 -0400

    addPattern action

--- src/actions/fetchPatterns.js
+++ src/actions/fetchPatterns.js
@@ -3,9 +3,22 @@
 export const fetchPatterns = () => {
     return (dispatch) => {
         dispatch({type: "LOADING_PATTERNS"})
-        return fetch('/patterns') //proxy used 
+        return fetch('/patterns')
         .then(res => res.json())
         .then(data => dispatch({type: "PATTERNS_LOADED", payload: data}))
+    }
+}
+
+export const addPattern = (pattern) => {
+    return (dispatch) => {
+        dispatch({ type: "ADD_PATTERN"}, pattern)
+        return fetch('/patterns', {
+            method: "POST",
+            body: JSON.stringify(pattern),
+            headers: {
+                'Content-Type': 'application'
+            }
+        })
     }
 }
 
