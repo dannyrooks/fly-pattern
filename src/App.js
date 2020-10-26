d89d3b60363ed1995a5aa09a34fd888dd392a253
Daniel Rooker <dannyrooks@gmail.com>
2020-10-25 22:37:06 -0400

    adds container and reducer folders

--- src/App.js
+++ src/App.js
@@ -12,17 +12,26 @@ class App extends React.Component {
     .then(data => console.log(data))
   }
 
-
   render(){
-
+  const patterns = this.props.patterns.map((pattern, index) => <li key={index}>{pattern.comment} </li>)
     return (
       <div className="App">
         <h1>
           Fly Pattern App
         </h1>
+        <ul>
+          {this.props.loading ? <h3>Loading.....</h3> : patterns}
+        </ul>
       </div>
     );
   }
 }
 
-export default connect()(App);
+const mapStateToProps = state => {
+  return {
+    patterns: state.patternReducer.patterns,
+    loading: state.patternReducer.loading
+  }
+}
+
+export default connect(mapStateToProps)(App);
