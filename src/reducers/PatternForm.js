ce6f065861e2eafe04585c18264684d9d72099f0
Daniel Rooker <dannyrooks@gmail.com>
2020-10-29 21:09:15 -0400

    patternForm reducer

---
+++ src/reducers/PatternForm.js
@@ -0,0 +1,23 @@
+const initialState = {
+    name: "",
+    category: "",
+    comment: ""
+}
+
+export default (state=initialState, action) => {
+    switch (action.type) {
+      case "UPDATE_PATTERN_FORM":
+        const returnVal = {
+          ...state,
+          [action.formData.name]: action.formData.value
+        }
+        return returnVal
+      case "RESET_PATTERN_FORM":
+        return initialState
+      case "SET_FORM_DATA_FOR_EDIT":
+        return action.patternFormData
+      default:
+        return state
+    }
+  }
+  
