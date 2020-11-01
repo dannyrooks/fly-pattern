9853ccdc1c5d294e674ff8c34cce7cb848a00066
Daniel Rooker <dannyrooks@gmail.com>
2020-11-01 12:10:43 -0500

    updates store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import PatternReducer from './reducers/PatternReducer'
 import PatternForm from './components/PatternForm'
+import Patterns from './components/Patterns'
 // import materialReducer from './reducers/materialReducer'
 import thunk from 'redux-thunk';
 
@@ -13,8 +14,7 @@ const reducer = combineReducers({
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
-
-
+ 
 
 export default store
 
