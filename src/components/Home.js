9b80eea7dc873ea07cf136cb60973ae8f9f08dc6
Daniel Rooker <dannyrooks@gmail.com>
2020-12-12 23:58:55 -0500

    image spinny

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,9 @@ import Image from 'react-bootstrap/Image'
 function Home() {
     
     return (
-        <Image className="image" src="https://www.tridentflyfishing.com/media/catalog/product/cache/972702a044b6fe55ba6d834a75577b47/_/u/_umpqua_wulff_royal_1_42.jpg" fluid />
+        <Image className="image" 
+        src="https://www.tridentflyfishing.com/media/catalog/product/cache/972702a044b6fe55ba6d834a75577b47/_/u/_umpqua_wulff_royal_1_42.jpg" fluid 
+        />
     )
 }
 
