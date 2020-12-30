610d8f64f2d7e6439ff5a12ae67bd69d9fd09aa2
Daniel Rooker <dannyrooks@gmail.com>
2020-12-30 12:24:32 -0500

    new commit

--- src/components/PatternCard.js
+++ src/components/PatternCard.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
-// import { Link } from 'react-router-dom'
-// import { fetchMaterials } from '../actions/materialActions'
 import MaterialForm from '../components/MaterialForm'
-// import Material from '../components/Material'
 import { fetchPatterns } from '../actions/fetchPatterns'
-// import Button from 'react-bootstrap/Button'
 import CardDeck from 'react-bootstrap/CardDeck'
-// import patternsReducer from '../reducers/PatternsReducer'
-
+ 
 class PatternCard extends React.Component {
 
     render() {
-        // const id = this.props.match.params
-        console.log(this.props.patterns[0]) // need to destructure?
+        const obj = this.props.patterns[0]
+        console.log(obj) // need to destructure?
         const patternCard = this.props.patterns.map(pattern =>
             <li key={pattern.id}>
                 {pattern.name}
